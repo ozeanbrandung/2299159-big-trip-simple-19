8953aaa9415d05a77fcc9a95339730d1314c26a9
Natalya Molchanova <natalia.v.molchanova@yandex.ru>
2023-01-11 08:53:37 +0400

    lesson 4 with mentor

--- src/maps.js
+++ src/maps.js
@@ -1,4 +1,4 @@
-import {FilterType, SortType} from './enums';
+import {FilterType, PointType, SortType} from './enums';
 
 /**
  * @type {Record<string,FilterCallback<PointAdapter>>}
@@ -20,3 +20,27 @@ export const sortCallbackMap = {
   [SortType.OFFERS]: () => 0
 };
 //если коллбэки для сортировки возвращают 0, значит ничо не сортируется
+
+export const pointTitleMap = {
+  [PointType.TAXI]: 'Taxi',
+  [PointType.BUS]: 'Bus',
+  [PointType.TRAIN]: 'Train',
+  [PointType.SHIP]: 'Ship',
+  [PointType.DRIVE]: 'Drive',
+  [PointType.FLIGHT]: 'Flight',
+  [PointType.CHECK_IN]: 'Check-in',
+  [PointType.SIGHTSEEING]: 'Sightseeing',
+  [PointType.RESTAURANT]: 'Restaurant'
+};
+
+// export const pointIconsMap = Object.fromEntries(
+//   Object.entries(PointType).map(
+//     ([key, value]) => [value, `img/icons/${value}.png`]
+//   )
+// );
+
+export const pointIconsMap = Object.fromEntries(
+  Object.values(PointType).map(
+    (value) => [value, `img/icons/${value}.png`]
+  )
+);
--- src/views/point-view.js
+++ src/views/point-view.js
@@ -3,40 +3,66 @@ import {html} from '../utils';
 import './point-view.css';
 
 export default class PointView extends View {
-  constructor() {
-    super();
+  /**
+   * @param {PointViewState} state
+   */
+  constructor(state) {
+    //в супере вызовется сразу же createHtml который мы тут переписали с нужной нам разметкой
+    //итак, эта разметка появится в dom
+    super(state);
 
     this.classList.add('trip-events__item');
+    //и уж потом к разметке мы добавляем верстку с офферами
+    this.setOffers(state.offers);
   }
 
   /**
-   * @override
+   * @param {OfferViewState} state
    */
-  createHtml() {
+  createOfferHtml(state) {
+    return html`
+      <li class="event__offer">
+        <span class="event__offer-title">${state.title}</span>
+        &plus;&euro;&nbsp;
+        <span class="event__offer-price">${state.price}</span>
+      </li>
+    `;
+  }
+
+  setOffers(states) {
+    const offersHtml = states.map(this.createOfferHtml).join(''); //массив строк с разметкой на выходе который джойним
+    this.querySelector('.event__selected-offers').innerHTML = offersHtml;
+  }
+
+  /**
+   * @param {PointViewState} state
+   */
+  createHtml(state) {
     return html`
       <div class="event">
-        <time class="event__date" datetime="2019-03-18">MAR 18</time>
+        <time class="event__date" datetime="${state.startDate}">${state.date}</time>
         <div class="event__type">
-          <img class="event__type-icon" width="42" height="42" src="img/icons/taxi.png" alt="Event type icon">
+          <img class="event__type-icon" width="42" height="42" src="${state.icon}" alt="Event type icon">
         </div>
-        <h3 class="event__title">Taxi Amsterdam</h3>
+        <h3 class="event__title">${state.title}</h3>
         <div class="event__schedule">
           <p class="event__time">
-            <time class="event__start-time" datetime="2019-03-18T10:30">10:30</time>
+            <time class="event__start-time" datetime="${state.startDate}">${state.startTime}</time>
             &mdash;
-            <time class="event__end-time" datetime="2019-03-18T11:00">11:00</time>
+            <time class="event__end-time" datetime="${state.endDate}">${state.endTime}</time>
           </p>
         </div>
         <p class="event__price">
-          &euro;&nbsp;<span class="event__price-value">20</span>
+          &euro;&nbsp;<span class="event__price-value">${state.basePrice}</span>
         </p>
         <h4 class="visually-hidden">Offers:</h4>
         <ul class="event__selected-offers">
-          <li class="event__offer">
-            <span class="event__offer-title">Order Uber</span>
-            &plus;&euro;&nbsp;
-            <span class="event__offer-price">20</span>
-          </li>
+          <!-- ИЗ-ЗА ЭКРАНИРОВАНИЯ МЫ СЮДА НЕ МОЖЕМ ВПИХНУТЬ ЦИКЛ!-->
+<!--          <li class="event__offer">-->
+<!--            <span class="event__offer-title">Order Uber</span>-->
+<!--            &plus;&euro;&nbsp;-->
+<!--            <span class="event__offer-price">20</span>-->
+<!--          </li>-->
         </ul>
         <button class="event__rollup-btn" type="button">
           <span class="visually-hidden">Open event</span>
